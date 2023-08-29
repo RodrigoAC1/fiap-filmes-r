bffd97cbc14f0405ee560e9a7f1a8b936993b1fe
RodrigoAC1 <rocavalheiro1@gmail.com>
2023-08-29 20:07:08 -0300

    main

--- src/app/page.js
+++ src/app/page.js
@@ -4,33 +4,46 @@ import Image from 'next/image'
 export default function Home() {
   return ( //JSX
     <>
-      <nav className="bg-slate-900 p-4">
-        <ul>
-          <li><a href="#"><h1 className="text-3xl font-bold">Fiap Filmes</h1></a></li>
+      <nav className="flex p-4 bg-green-700">
+        <ul className="flex gap-20">
+          <li>
+            <a href="#">
+              <h1>Fiap Music</h1>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              favoritos
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Musicas
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Podcast
+            </a>
+          </li>
         </ul>
-        <ul>
-          <li><a href="#">favoritos</a></li>
-        </ul>
-        <ul>
-          <li><a href="#">filmes</a></li>
-        </ul>
-        
+
       </nav>
 
      <Title>em alta</Title>
 
       <div id="card" className='flex flex-col w-40 justify-center items-center m-2'>
         <img className='rounded' src="https://place-hold.it/150x220/666" alt="" />
-        <span className='font-bold text-center line-clamp-1'>titulo do filme do card</span>
+        <span className='font-bold text-center line-clamp-1'>Nome da musica</span>
         <div>
           <span>6.0</span>
         </div>
-        <a href="#" className='bg-pink-600 py-2 w-full rounded text-center'>
+        <a href="#" className='bg-red-600 py-2 w-full rounded text-center'>
           detalhes
         </a>
       </div>
 
-     <Title>lançamentos</Title>
+     <Title>Novas</Title>
      <Title>favoritos</Title>
    
     </>
