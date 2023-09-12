abedc33abe57c633768fd6f04e6b7427de349825
RodrigoAC1 <rocavalheiro1@gmail.com>
2023-09-12 20:13:29 -0300

    main

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,28 @@
-import CardFilme from '@/components/Cardmusic'
+import CardMusic from '@/components/CardMusic'
 import Title from '@/components/Title'
 
-export default function Home() {
-  const filmes = [
-    {
-      titulo: "Anos Luz",
-      nota: 9.5,
-      poster: "https://th.bing.com/th/id/OIP.fiADlluS74qB0Gn4vCfDUAHaHa?w=160&h=180&c=7&r=0&o=5&pid=1.7"
-    },
-    {
-      titulo: "Cartão black",
-      nota: 8.5,
-      poster: "https://th.bing.com/th/id/OIP.qU79_MPl1mg7A8OOgBwVWgHaHa?w=186&h=186&c=7&r=0&o=5&pid=1.7"
-    }
-  ]
+async function carregarMusics(){
+  const url = "https://raw.githubusercontent.com/KoreanThinker/billboard-json/main/billboard-hot-100/recent.json"
+  const resposta = await fetch(url)
+  const json = await resposta.json()
+  return json.data
+}
+
+export default async function Home() {
+  
+  const Music = await carregarMusics()
 
   return ( //JSX
     <>
       <nav className="bg-green-700 p-4">
         <ul>
-          <li><a href="#"><h1 className="text-3xl font-bold">Fiap Musics</h1></a></li>
+          <li><a href="#"><h1 className="text-3xl font-bold">Fiap Music</h1></a></li>
         </ul>
         <ul>
           <li><a href="#">favoritos</a></li>
         </ul>
         <ul>
-          <li><a href="#">Musicas</a></li>
+          <li><a href="#">filmes</a></li>
         </ul>
         
       </nav>
@@ -33,7 +30,7 @@ export default function Home() {
      <Title>em alta</Title>
 
       <section className='flex flex-wrap'>
-        {filmes.map(filme => <CardFilme filme={filme} /> )}
+        {Music.map(Music=> <CardMusic music={Music} /> )}
       </section>
 
      <Title>lançamentos</Title>
--- src/components/CardMusic.jsx
+++ src/components/CardMusic.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 
 export default function CardMusic({music}){
     const [favorito, setFavorito] = useState(true)
+    const url_imagem = `${music.image}`
 
     return (
         <div id="card" className='flex flex-col w-40 justify-center items-center m-2'>
@@ -13,12 +14,12 @@ export default function CardMusic({music}){
             :
                 <HeartIcon onClick={() => setFavorito(true)}  className="h-6 w-6 text-slate-100 cursor-pointer" />
             }
-            <img className='rounded' src={music.poster} alt="" />
+            <img className='rounded' src={url_imagem} alt="" />
             <span className='font-bold text-center line-clamp-1'>
-                {music.titulo}
+                {music.name}
             </span>
             <div>
-                <span>{music.nota}</span>
+                <span>{music.rank}°</span>
             </div>
             <a href="#" className='bg-pink-600 py-2 w-full rounded text-center'>
                 detalhes
